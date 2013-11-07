pangratz/ember.js

commit cf0caa04f625067ec3b4629acdec12c8517df94e

[BUGFIX beta] sync router.js
- Sync router.js
- Don't fire `error` actions on transitions that have already been
  aborted/redirected
- Closes #3162

packages/ember-testing/tests/acceptance_test.js
@@ -86,12 +86,9 @@ test("helpers can be chained with then", function() {
   }).then(function() {
     equal(Ember.$('.ember-text-field').val(), 'context working', "chained with fillIn");
     click(".does-not-exist");
-  }).then(function() {
-    // This redundant `then` is needed in this test
-    // so we can assert that thrown exceptions
-    // do not fire multiple times
+  }).then(null, function(e) {
+    equal(e.message, "Element .does-not-exist not found.", "Non-existent click exception caught");
   });
-
 });
 
 

packages/ember-routing/lib/system/router.js
@@ -3,7 +3,7 @@
 @submodule ember-routing
 */
 
-var Router = requireModule("router");
+var Router = requireModule("router")['default'];
 var get = Ember.get, set = Ember.set;
 var defineProperty = Ember.defineProperty;
 

packages/ember-routing/lib/vendor/router.js
@@ -1,6 +1,6 @@
 define("router",
-  ["route-recognizer","rsvp"],
-  function(RouteRecognizer, RSVP) {
+  ["route-recognizer","rsvp","exports"],
+  function(__dependency1__, __dependency2__, __exports__) {
     "use strict";
     /**
       @private
@@ -20,6 +20,8 @@ define("router",
       * `{Object} context`: the active context for the handler
     */
 
+    var RouteRecognizer = __dependency1__;
+    var RSVP = __dependency2__;
 
     var slice = Array.prototype.slice;
 
@@ -174,6 +176,7 @@ define("router",
     // TODO: separate into module?
     Router.Transition = Transition;
 
+    __exports__['default'] = Router;
 
 
     /**
@@ -529,7 +532,7 @@ define("router",
     }
 
     function isParam(object) {
-      return (typeof object === "string" || object instanceof String || !isNaN(object));
+      return (typeof object === "string" || object instanceof String || typeof object === "number" || object instanceof Number);
     }
 
 
@@ -702,7 +705,7 @@ define("router",
         // Make sure this route is actually accessible by URL.
         for (i = 0, len = results.length; i < len; ++i) {
 
-          if (router.getHandler(results[i].handler).inaccessiblyByURL) {
+          if (router.getHandler(results[i].handler).inaccessibleByURL) {
             results = null;
             break;
           }
@@ -1069,14 +1072,7 @@ define("router",
         checkAbort(transition);
 
         try {
-          log(router, transition.sequence, "Validation succeeded, finalizing transition;");
-
-          // Don't overwrite contexts / update URL if this was a noop transition.
-          if (!currentHandlerInfos || !currentHandlerInfos.length ||
-              !router.recognizer.hasRoute(currentHandlerInfos[currentHandlerInfos.length - 1].name) ||
-              currentHandlerInfos.length !== matchPointResults.matchPoint) {
-            finalizeTransition(transition, handlerInfos);
-          }
+          finalizeTransition(transition, handlerInfos);
 
           // currentHandlerInfos was updated in finalizeTransition
           trigger(router, router.currentHandlerInfos, true, ['didTransition']);
@@ -1161,6 +1157,8 @@ define("router",
      */
     function finalizeTransition(transition, handlerInfos) {
 
+      log(transition.router, transition.sequence, "Validation succeeded, finalizing transition;");
+
       var router = transition.router,
           seq = transition.sequence,
           handlerName = handlerInfos[handlerInfos.length - 1].name,
@@ -1176,7 +1174,7 @@ define("router",
           objects.unshift(isParam(providedModel) ? providedModel.toString() : handlerInfo.context);
         }
 
-        if (handlerInfo.handler.inaccessiblyByURL) {
+        if (handlerInfo.handler.inaccessibleByURL) {
           urlMethod = null;
         }
       }
@@ -1263,7 +1261,7 @@ define("router",
       }
 
       function handleError(reason) {
-        if (reason instanceof Router.TransitionAborted) {
+        if (reason instanceof Router.TransitionAborted || transition.isAborted) {
           // if the transition was aborted and *no additional* error was thrown,
           // reject with the Router.TransitionAborted instance
           return RSVP.reject(reason);
@@ -1450,7 +1448,4 @@ define("router",
       }
       return object;
     }
-
-
-    return Router;
   });

commit 31bbe1e7b29b0bfee4b4e442afbc7f7356c45709

Add spec for unhandled exception being logged by Test.adapter.exception
The changes in exception handling introduced in
cf0caa04f625067ec3b4629acdec12c8517df94e made a test assertion in an
existing test obsolete, since it wasn't called anymore.

This commit adds a spec for Test.adapter.exception being invoked, when
an exception is not handled.

packages/ember-testing/tests/acceptance_test.js
@@ -70,11 +70,6 @@ module("ember-testing Acceptance", {
 
 test("helpers can be chained with then", function() {
   expect(5);
-  Ember.Test.adapter = Ember.Test.QUnitAdapter.create({
-    exception: function(error) {
-      equal(error.message, "Element .does-not-exist not found.", "Exception successfully caught and passed to Ember.Test.adapter.exception");
-    }
-  });
 
   currentRoute = 'index';
 
@@ -192,4 +187,22 @@ test("Helpers nested in thens", function() {
   andThen(function() {
     equal(currentRoute, 'posts');
   });
+});
+
+test("Unhandled exceptions are logged via Ember.Test.adapter#exception", function () {
+  expect(2);
+
+  Ember.Test.adapter = Ember.Test.QUnitAdapter.create({
+    exception: function(error) {
+      equal(error.message, "Element .does-not-exist not found.", "Exception successfully caught and passed to Ember.Test.adapter.exception");
+    }
+  });
+
+  visit('/posts');
+
+  click(".invalid-element").then(null, function(error) {
+    equal(error.message, "Element .invalid-element not found.", "Exception successfully handled in the rejection handler");
+  });
+
+  click(".does-not-exist");
 });